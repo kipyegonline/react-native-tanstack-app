7226e3e7bd9dc0337d5b56d330c949aa72007b29
Vince <vinnykipx@gmail.com>
2024-08-07 16:29:26 +0300

    landing page

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, StatusBar as SB } from "react-native";
+import Welcome from "./components/Welcome";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Welcome />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    paddingTop: SB.currentHeight,
+    //alignItems: "center",
+    //justifyContent: "center",
   },
 });
--- components/Welcome.tsx
+++ components/Welcome.tsx
@@ -13,21 +13,52 @@ export default function Welcome() {
     <ImageBackground
       source={require("../assets/Screenshot from 2019-11-25 23-20-34.png")}
       style={styles.container}
+      //resizeMode="contain"
     >
+      <View style={{ position: "absolute", top: "40%" }}>
+        <Text style={styles.welcome}>Welcome to the Spring Valley</Text>
+      </View>
       <View style={styles.buttonContainer}>
-        <TouchableOpacity>
-          <Text>Login</Text>
+        <TouchableOpacity style={styles.loginBtn} activeOpacity={0.8}>
+          <Text style={styles.btnText}>Login</Text>
         </TouchableOpacity>
-        <TouchableOpacity>
-          <Text>Sign up</Text>
+        <TouchableOpacity style={styles.signBtn} activeOpacity={0.8}>
+          <Text style={styles.btnText}>Sign up</Text>
         </TouchableOpacity>
       </View>
-      <Text>Welcome</Text>
     </ImageBackground>
   );
 }
 const styles = StyleSheet.create({
-  container: { flex: 1 },
+  container: { flex: 1, paddingTop: 20, backgroundColor: "rgba(0,0,0,.9)" },
+  welcome: {
+    color: "white",
+    fontWeight: "700",
+    fontSize: 44,
+    textAlign: "center",
+    textTransform: "capitalize",
+    paddingHorizontal: 4,
+  },
   logoContainer: {},
-  buttonContainer: { flex: 1, justifyContent: "flex-end" },
+  buttonContainer: {
+    flex: 1,
+    justifyContent: "flex-end",
+    gap: 10,
+    marginBottom: 30,
+    paddingHorizontal: 10,
+  },
+  loginBtn: {
+    width: "100%",
+    borderRadius: 12,
+    padding: 14,
+    backgroundColor: "hotpink",
+  },
+  signBtn: {
+    width: "100%",
+    borderRadius: 12,
+    padding: 12,
+    borderWidth: 1,
+    borderColor: "hotpink",
+  },
+  btnText: { color: "white", fontWeight: "700", textAlign: "center" },
 });
